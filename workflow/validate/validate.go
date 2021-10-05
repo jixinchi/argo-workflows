9e4467927ae8811af6ffd80c5de042589cdd4d86
jixinchi <jixinchi@bytedance.com>
2021-10-05 09:20:31 +0800

    feat: separately process output artifacts of parallel pods
    Signed-off-by: jixinchi <jixinchi@bytedance.com>

--- workflow/validate/validate.go
+++ workflow/validate/validate.go
@@ -896,6 +896,8 @@ func (ctx *templateValidationCtx) addOutputsToScope(tmpl *wfv1.Template, prefix 
 			scope[fmt.Sprintf("%s.exitCode", prefix)] = true
 		default:
 			scope[fmt.Sprintf("%s.outputs.parameters", prefix)] = true
+			scope[fmt.Sprintf("%s.outputs.artifacts", prefix)] = true
+			scope[fmt.Sprintf("%s.outputs", prefix)] = true
 		}
 	}
 	if isAncestor {
