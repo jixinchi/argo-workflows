ca9cd859307f9c17ffb82a5166cd123307fb4cc3
jixinchi <jixinchi@bytedance.com>
2021-10-09 20:05:16 +0800

    test: add test for param
    Signed-off-by: jixinchi <jixinchi@bytedance.com>

--- pkg/apis/workflow/v1alpha1/item_test.go
+++ pkg/apis/workflow/v1alpha1/item_test.go
@@ -11,12 +11,14 @@ import (
 
 func TestItem(t *testing.T) {
 	for data, expectedType := range map[string]Type{
-		"0":                               Number,
-		"3.141":                           Number,
-		"true":                            Bool,
-		"\"hello\"":                       String,
-		"{\"val\":\"123\"}":               Map,
-		"[\"1\",\"2\",\"3\",\"4\",\"5\"]": List,
+		"0":                                                          Number,
+		"3.141":                                                      Number,
+		"true":                                                       Bool,
+		"\"hello\"":                                                  String,
+		"{\"val\":\"123\"}":                                          Map,
+		"[\"1\",\"2\",\"3\",\"4\",\"5\"]":                            List,
+		"{\"name\":\"test\",\"path\":\"/tmp/path\"}":                 ArtifactType,
+		"{\"parameters\":[{\"name\":\"test\",\"value\":\"value\"}]}": OutputsType,
 	} {
 		t.Run(fmt.Sprintf("%v", expectedType), func(t *testing.T) {
 			t.Run("Item", func(t *testing.T) {
@@ -61,6 +63,20 @@ func TestItem_GetStrVal(t *testing.T) {
 	MustUnmarshal([]byte(`"foo"`), &item)
 	val := item.GetStrVal()
 	assert.Equal(t, "foo", val)
+}
+
+func TestItem_GetArtifactVal(t *testing.T) {
+	item := Item{}
+	MustUnmarshal([]byte(`{"name": "test", "path": "/tmp/path"}`), &item)
+	val := item.GetArtifactVal()
+	assert.Equal(t, Artifact{Name: "test", Path: "/tmp/path"}, val)
+}
+
+func TestItem_GetOutputsVal(t *testing.T) {
+	item := Item{}
+	MustUnmarshal([]byte(`{"parameters": [{"name": "test", "value":"value"}]}`), &item)
+	val := item.GetOutputsVal()
+	assert.Equal(t, Outputs{Parameters: []Parameter{{Name: "test", Value: AnyStringPtr("value")}}}, val)
 }
 
 var testItemStringTable = []struct {
